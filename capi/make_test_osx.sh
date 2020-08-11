8a3427e06d5ebb2dd569379ceeb265d5dc224da1
C.S <cryptonote.social@gmail.com>
2020-08-11 08:17:40 -0700

    set install_name for osx dylib

--- capi/make_test_osx.sh
+++ capi/make_test_osx.sh
@@ -1,3 +1,3 @@
-go build -o capi.dynlib -buildmode=c-shared capi.go
+go build -o capi.dynlib -buildmode=c-shared -ldflags="-extldflags=-Wl,-install_name,@rpath/capi.dynlib" capi.go
 gcc -c test.c -o test.o
-gcc test.o capi.dynlib -o test
+gcc test.o capi.dynlib -rpath `pwd` -o test
