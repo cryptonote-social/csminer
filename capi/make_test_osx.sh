35602793b4673ecdbd497092bc29457ca77d6f57
C.S <cryptonote.social@gmail.com>
2020-08-11 13:03:34 -0700

    update make_test_osx for start_miner

--- capi/make_test_osx.sh
+++ capi/make_test_osx.sh
@@ -1,3 +1,4 @@
 go build -o capi.dynlib -buildmode=c-shared -ldflags="-extldflags=-Wl,-install_name,@rpath/capi.dynlib" capi.go
+
 gcc -c test.c -o test.o
-gcc test.o capi.dynlib -rpath `pwd` -o test
+gcc  -L../../rxlib/ test.o capi.dynlib ../../rxlib/rxlib.cpp.o -lrandomx -lstdc++ -lm -rpath `pwd` -o test
