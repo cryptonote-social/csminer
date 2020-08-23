d34e2d3d31687824d911c36557e080bfee9cbd0e
C.S <cryptonote.social@gmail.com>
2020-08-22 18:03:22 -0700

    fix size-reducing flags

--- capi/make_test_osx.sh
+++ capi/make_test_osx.sh
@@ -1,4 +1,4 @@
-go build -a -ldflags="-s -w" -o capi.dynlib -buildmode=c-shared -ldflags="-extldflags=-Wl,-install_name,@rpath/capi.dynlib" capi.go
+go build -a -o capi.dynlib -buildmode=c-shared -ldflags="-extldflags=-Wl,-install_name,@rpath/capi.dynlib -s -w" capi.go
 
-gcc -c test.c -o test.o
-gcc  -L../../rxlib/ test.o capi.dynlib ../../rxlib/rxlib.cpp.o -lrandomx -lstdc++ -lm -rpath `pwd` -o test
+gcc -O3 -c test.c -o test.o
+gcc -O3 -L../../rxlib/ test.o capi.dynlib ../../rxlib/rxlib.cpp.o -lrandomx -lstdc++ -lm -rpath `pwd` -o test
